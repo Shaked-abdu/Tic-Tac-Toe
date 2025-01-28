97a17558f52747792f8bfc04e3d4b7458436122d
Shaked <shakedabdu50@gmail.com>
2025-01-28 15:29:08 +0200

    fix typo effects

--- dist/utils/start-state.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.startState = void 0;
-exports.startState = {
-    board: Array(9).fill(""),
-    currentPlayer: "X",
-    winner: null,
-};
